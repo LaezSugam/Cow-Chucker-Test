7cfa03cdf1c01c59f2e8a5c32e90be460ff180cb
Bill Peterson <william.o.peterson@gmail.com>
2017-04-25 10:38:08 -0700

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject cow;
+	public Transform cowSpawn;
+
+	void Update(){
+
+
+		if (Input.GetButton ("Fire1")) {
+			//GameObject clone = 
+			Instantiate(cow, cowSpawn.position, cowSpawn.rotation);// as GameObject;
+
+		}
+	}
+}
