9f14503cca93817f83ad3e2e46f3e8ee4441dcbc
Bill Peterson <william.o.peterson@gmail.com>
2017-04-26 17:14:17 -0700

    Ragdoll maybe

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,14 +9,33 @@ public class PlayerController : MonoBehaviour {
 
 	public float fireRate = 0.5f;
 	private float nextFire = 0.0f;
+	private int thrust = 1;
+	public int thrustMultiplier = 1;
+
+	public GameObject cam1;
+	public GameObject cam2;
+
+
+	void Start(){
+//		cam1.SetActive = true;
+//		cam2.SetActive = false;
+	}
 
 	void Update(){
 
 
 		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
-			nextFire = Time.time + fireRate;
-			Instantiate(cow, cowSpawn.position, cowSpawn.rotation);
+			thrust += thrustMultiplier;
 
+		}
+		if (Input.GetButtonUp ("Fire1")) {
+			GameObject cowInstance = Instantiate(cow, cowSpawn.position, cowSpawn.rotation);
+			Rigidbody cowRB = cowInstance.GetComponent<Rigidbody> ();
+			cowRB.AddForce (cowSpawn.transform.forward * thrust);
+
+//			GameObject cowHips = cowInstance.Find ("Character1_Hips") as GameObject;
+//			Rigidbody cowRB = cowHips.GetComponent<Rigidbody> ();
+//			cowRB.AddForce (cowSpawn.transform.forward * thrust);
 		}
 	}
 }
